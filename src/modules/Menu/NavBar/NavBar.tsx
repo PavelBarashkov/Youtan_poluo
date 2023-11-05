85441afbaba7ddaabecc652dcb6cfe3790b722d4
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-11-05 14:09:18 +0400

    switch page store and updata menu for other page

--- src/modules/Menu/NavBar/NavBar.tsx
+++ src/modules/Menu/NavBar/NavBar.tsx
@@ -5,11 +5,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import { IconBasket } from '../../../UI/IconBasket/IconBasket';
 import { Logo } from '../../../UI/Logo/Logo';
 import classesNavBar from './navbar.module.css'
+import { useLocation, useNavigate } from 'react-router-dom';
+import { MAIN_ROUTE, STORE_ROUTE } from '../../../routes/consts';
 
 
 export const NavBar = () => {
     const [sizeWindow, setSizeWindow] = useState<number>(window.innerWidth);
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+    const location = useLocation();
+    const isMain = location.pathname === MAIN_ROUTE;
+    const navigate = useNavigate();
 
     useEffect(() => {
         const handleResize = () => {
@@ -38,17 +43,17 @@ export const NavBar = () => {
     if (sizeWindow < 1200) {
         return (
             <>
-            <Navbar expand="xl" className={isMenuOpen ? classesNavBar.customNavbarMenuOpen : classesNavBar.customNavbar} data-bs-theme="dark" >
+            <Navbar expand="xl" className={isMenuOpen ? classesNavBar.customNavbarMenuOpen : classesNavBar.customNavbarMain} data-bs-theme="dark" >
                 <Container className="">
 
-                    <Navbar.Brand className='p-0' href="#home"><Logo/></Navbar.Brand>
+                    <Navbar.Brand className={`p-0 ${classesNavBar.logo}`} onClick={() => navigate(MAIN_ROUTE)}><Logo/></Navbar.Brand>
 
                     <Navbar.Toggle onClick={toggleMenu} aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav className={`d-flex  ${classesNavBar.customContainerNav}`}>
                             <Nav className={`d-flex  ${classesNavBar.customContainerNav}`}>
 
-                                <Nav.Link className={classesNavBar.customNavLink} href="#home">Магазин</Nav.Link>
+                                <Nav.Link className={classesNavBar.customNavLink} onClick={() => navigate(STORE_ROUTE)}>Магазин</Nav.Link>
                                 <Nav.Link className={classesNavBar.customNavLink} href="#features">О бренде</Nav.Link>
                                 <Nav.Link className={classesNavBar.customNavLink} href="#pricing">Контакты</Nav.Link>
 
@@ -66,19 +71,43 @@ export const NavBar = () => {
         )
     }
 
+    if (isMain) {
+        return (
+            <>
+            <Navbar className={classesNavBar.customNavbarMain} data-bs-theme="dark" >
+                <Container className="d-flex justify-content-around">
+                        <Nav className={`mo-auto gap-3 ${classesNavBar.customNav}`}>
+                            <Nav.Link className={classesNavBar.customNavLink} onClick={() => navigate(STORE_ROUTE)}>Магазин</Nav.Link>
+                            <Nav.Link className={classesNavBar.customNavLink} href="#features">О бренде</Nav.Link>
+                            <Nav.Link className={classesNavBar.customNavLink} href="#pricing">Контакты</Nav.Link>
+                        </Nav>
 
+                        <Navbar.Brand className={`${classesNavBar.logo}`} onClick={() => navigate(MAIN_ROUTE)}><Logo/></Navbar.Brand>
+
+                        <Nav className={`mo-auto gap-3 ${classesNavBar.customNav}`}>
+                            <Nav.Link className={classesNavBar.customNavLink}  href="#home">Доставка и оплата</Nav.Link>
+                            <Nav.Link className={classesNavBar.customNavLink}  href="#features">FAQ</Nav.Link>
+                            <Nav.Link className={classesNavBar.customNavLink}  href="#pricing">
+                                <IconBasket />
+                            </Nav.Link>
+                        </Nav>
+                </Container>
+            </Navbar>
+        </>
+        )
+    }
 
     return (
         <>
             <Navbar className={classesNavBar.customNavbar} data-bs-theme="dark" >
                 <Container className="d-flex justify-content-around">
                         <Nav className={`mo-auto gap-3 ${classesNavBar.customNav}`}>
-                            <Nav.Link className={classesNavBar.customNavLink} href="#home">Магазин</Nav.Link>
+                            <Nav.Link className={classesNavBar.customNavLink} onClick={() => navigate(STORE_ROUTE)}>Магазин</Nav.Link>
                             <Nav.Link className={classesNavBar.customNavLink} href="#features">О бренде</Nav.Link>
                             <Nav.Link className={classesNavBar.customNavLink} href="#pricing">Контакты</Nav.Link>
                         </Nav>
 
-                        <Navbar.Brand href="#home"><Logo/></Navbar.Brand>
+                        <Navbar.Brand className={`${classesNavBar.logo}`} onClick={() => navigate(MAIN_ROUTE)}><Logo/></Navbar.Brand>
 
                         <Nav className={`mo-auto gap-3 ${classesNavBar.customNav}`}>
                             <Nav.Link className={classesNavBar.customNavLink}  href="#home">Доставка и оплата</Nav.Link>
