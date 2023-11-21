2810539f49ffb4258b511d0cb78c8c3696e8b058
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-11-21 16:55:13 +0400

    fix bux footer position

--- src/modules/Shope/API/fetchCards.ts
+++ src/modules/Shope/API/fetchCards.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function fetchCards() {
-    const response = await axios.get('http://localhost:5000/api/card/mini');
+    const response = await axios.get('http://localhost:5000/api/card/model/mini');
     return response
 }
