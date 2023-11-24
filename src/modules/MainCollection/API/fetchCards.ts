a3232fc9a6605c2039837602f6554f9f70d84a34
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-11-24 18:43:13 +0400

    added error handling

--- src/modules/MainCollection/API/fetchCards.ts
+++ src/modules/MainCollection/API/fetchCards.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function fetchCards() {
-    const response = await axios.get('http://localhost:5000/api/card/mini/favorite');
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/card/model/mini/favorite`);
     return response
 }
--- src/modules/MainCollection/store/slice/cardsSlice.tsx
+++ src/modules/MainCollection/store/slice/cardsSlice.tsx
@@ -1,66 +1,72 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { fetchCards } from '../../API/fetchCards';
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { fetchCards } from "../../API/fetchCards";
 
-
+interface ICard {
+  cardId: number;
+  modelId: number;
+  name: string;
+  price: number;
+  colors: string[];
+  sizes: string[];
+  imgs: string[];
+}
 
 interface CardsState {
-    cards?: {
-        cardId?: number,
-        modelId?: number,
-        name?: string,
-        price?: number,
-        colors?: string[],
-        sizes?: string[],
-        imgs?: string[]
-    },
-    loading: boolean,
-    error: string,
-    
+  cards: ICard[];
+  loading: boolean;
+  error: string;
 }
 
 const initialState: CardsState = {
-    cards: {},
-    loading: false,
-    error: '',
-}
-  
+  cards: [],
+  loading: false,
+  error: "",
+};
 
-export const fetchCard = createAsyncThunk(
-    'cars/fetchCards',
-    async () => {
-        try {
-            const response = await fetchCards();
-            return response.data;
-        } catch (e) {
-            console.log(e)
-        }
-    }
-  );
+export const fetchCard = createAsyncThunk("cars/fetchCards", async () => {
+    const response = await fetchCards();
+    return response.data;
   
+});
 
 export const cardsSlice = createSlice({
-    name: 'cards',
-    initialState,
-    reducers: {
-    },
-    extraReducers: (builder) => {
-        builder
-        .addCase(fetchCard.pending, (state) => {
-            state.loading = true;
-            state.error = '';
-        })
-        .addCase(fetchCard.fulfilled, (state, action) => {
-            state.cards = action.payload;
-            state.loading = false;
-            state.error = '';
-        })
-        .addCase(fetchCard.rejected, (state, action) => {
-            console.log("Error:", action.payload);
-            state.loading = false;
-            state.error = action.payload as string;
-        });
-    }
-})
+  name: "cards",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCard.pending, (state) => {
+        state.loading = true;
+        state.error = "";
+      })
+      .addCase(fetchCard.fulfilled, (state, action) => {
+        state.cards = action.payload;
+        state.loading = false;
+        state.error = "";
+      })
+      .addCase(fetchCard.rejected, (state, action) => {
+        console.log("Error:", action.payload);
+        state.loading = false;
 
-export const { } = cardsSlice.actions;
+        if (action.payload instanceof Error) {
+          state.error = action.payload.message;
+        } else if (typeof action.payload === "string") {
+          try {
+            const errorData = JSON.parse(action.payload);
+            if (errorData.message) {
+              state.error = errorData.message;
+            } else {
+              state.error = "Произошла ошибка при загрузке.";
+            }
+          } catch (error) {
+            state.error = "Произошла ошибка при загрузке.";
+          }
+        } else {
+          state.error = "Произошла ошибка при загрузке.";
+        }
+      });
+  },
+});
+
+export const {} = cardsSlice.actions;
 export default cardsSlice.reducer;
